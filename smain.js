9c8aa1255cec310dcfb1325765d480ec6202aa45
The Sewn Identity <179808961+The-Sewn-Identity@users.noreply.github.com>
2025-12-01 17:04:46 +0100

    Added smain.js

---
+++ smain.js
@@ -0,0 +1,13 @@
+import { createServer } from 'http';
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/html; charset=UTF-8'});
+    res.end('hey.');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
